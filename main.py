2da5b7234849a3d762ba0a7127a4353f0dc2387b
Peter Radakovits <peter.radakovits@gmail.com>
2025-04-14 14:38:23 +0200

    remove hardcoded path to book, add functionality to add path via CLI

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words, count_chars, sort_dict
 
 def get_book_text(path):
@@ -23,7 +24,12 @@ def print_report(book_path, word_count, sorted_list):
 
 def main():
 
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    # book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     word_count = count_words(book_text)
     text_chars = count_chars(book_text)
